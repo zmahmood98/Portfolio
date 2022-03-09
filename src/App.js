435b6b948c9b07f5f287b798a470ac5300461816
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-09 19:22:28 +0000

    add basic structure

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+import './App.css';
+import { Home, Background, Hobbies, Contact } from './pages';
+import { Header, Footer } from './layout';
+
+function App() {
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/background">
+            <Background />
+          </Route>
+          <Route path="/interests-and-hobbies">
+            <Hobbies />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
+  
+  export default App;
