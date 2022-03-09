102cfedffdb16e900c33f5cfe0592e4089d01c87
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-09 23:02:43 +0000

    update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -35,6 +35,7 @@ const config = {
     new HtmlWebpackPlugin({
       // used to add the JavaScript code to the HTML
       template: path.join(PUBLIC_DIRECTORY, "index.html"),
+      favicon: './public/favicon.ico',
     }),
   ],
   module: {
