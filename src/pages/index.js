3f135bc075c432fb2daf5676c066cc3cb3743fa9
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-09 19:34:59 +0000

    add pages components

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Background } from './Background';
+export { default as Hobbies } from './Hobbies';
+export { default as Contact } from './Contact';
