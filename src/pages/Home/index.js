d061d065879366c2f15874c80ff3827a0d0120e4
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-09 23:14:43 +0000

    update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,7 @@ function Home () {
     return (
         <>
             <img src={clouds} alt="clouds" className="cloud-image" />
-                <div className='about-div'>
+                <div className='home-div'>
                     <h1 className="home-h1">ZAHRA MAHM<div className='oo'>oo</div>D</h1>
                     <h3>Welcome to my portfolio!</h3>   
                 </div>
