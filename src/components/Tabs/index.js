7daef929e9d7e6c7925456bc952cc0dd25e3b686
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-10 13:14:28 +0000

    add tabs component

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './index.css';
+
+const Tabs = () => {
+    
+    return (
+        <>
+            <section className="tab-section">
+            <div className="rt-container">
+                <div className="col-rt-12">
+                    
+                    <div className="conveytabs">
+                    <input id="tab1" type="radio" name="tabs"/>
+                    <label for="tab1">Education</label>
+                        
+                    <input id="tab2" type="radio" name="tabs" />
+                    <label for="tab2">Experience</label>
+                        
+                    <input id="tab3" type="radio" name="tabs" />
+                    <label for="tab3">Achievements</label>
+    
+                    <input id="tab4" type="radio" name="tabs" />
+                    <label for="tab4">Aspirations</label>
+                        
+                    <section id="content1">
+                        <h2>My Education</h2>
+                        
+                        <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas iure, ullam porro incidunt velit assumenda in expedita totam ipsa accusantium dignissimos quidem similique veniam mollitia? Incidunt, suscipit? Assumenda, amet repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, incidunt voluptates. Cum porro animi optio minima totam debitis temporibus, consequatur incidunt doloremque. Cum quam reprehenderit ad tempora ex nostrum accusantium.
+                        </p>
+                        <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, error quae. Fugit blanditiis quam atque, ut, itaque ipsam facilis distinctio nostrum recusandae ex odit fugiat, tempore exercitationem ea ipsa dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, tempore. Voluptatem eos inventore nemo atque perferendis reprehenderit laudantium quisquam tempore quaerat nulla repellendus provident, minus soluta perspiciatis accusantium, officiis omnis?
+                        </p>
+                    </section>
+                        
+                    <section id="content2"> 
+                    <h2>My Experience</h2>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, error quae. Fugit blanditiis quam atque, ut, itaque ipsam facilis distinctio nostrum recusandae ex odit fugiat, tempore exercitationem ea ipsa dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, tempore. Voluptatem eos inventore nemo atque perferendis reprehenderit laudantium quisquam tempore quaerat nulla repellendus provident, minus soluta perspiciatis accusantium, officiis omnis?
+                        </p>
+                    </section>
+    
+                    <section id="content3">
+                    <h2>My Achievements</h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas iure, ullam porro incidunt velit assumenda in expedita totam ipsa accusantium dignissimos quidem similique veniam mollitia? Incidunt, suscipit? Assumenda, amet repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, incidunt voluptates. Cum porro animi optio minima totam debitis temporibus, consequatur incidunt doloremque. Cum quam reprehenderit ad tempora ex nostrum accusantium.
+                        </p>
+                    </section>
+                        
+                    <section id="content4">
+                    <h2>My Aspirations</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt cumque consectetur neque eum maxime voluptas obcaecati aperiam assumenda iure, sed nobis, provident tempore rem sit fugit aliquam accusamus quos ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro expedita voluptatibus nam qui, tenetur eius sint officiis nostrum numquam quod laborum fuga. Perferendis, distinctio? Aliquam a sed at hic! In!
+                        </p>
+                    </section>
+                        
+                    </div>
+                
+            
+                </div>
+            </div>
+        </section>
+        <br /><br /><br />
+    
+        </>
+    )
+
+}
+
+export default Tabs;
+
