f2f74fa283a81233e9a88f7583157566c18edcb9
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-11 12:07:16 +0000

    update project page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './index.css';
 import construction from "../../images/construction.png"
+import { ProjectBox } from '../../components';
 
 function Projects () {
     return (
-      <div className='works-div'>
-       <img src={construction} alt="construction" className="construction-image" />  
-       <br></br>      
+      <div className='projects-div'>
+        <h2 className='projects-h2'>Projects I have been part of</h2>
+        <p className='projects-p'>Click on a project to be taken to it's GitHub page!</p>
+        <br></br>
+        <ProjectBox />      
       </div>
     );
   }
   
-  export default Projects;
+export default Projects;
