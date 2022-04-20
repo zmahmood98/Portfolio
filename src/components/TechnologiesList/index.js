d26f5605950ab63e76c86c1a787586407f75cdfc
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-04-20 22:25:02 +0100

    update

--- src/components/TechnologiesList/index.js
+++ src/components/TechnologiesList/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import Carousel from 'react-elastic-carousel';
 import './index.css';
+import database from '../../images/database.png'
 
 
-const TechnologiesList = () => {
+class TechnologiesList extends React.Component {
 
-    return (
-        <>
-        
-        </>
+    state = {
+        items: [
+          {id: 1, title: 'MongoDB', image: database}
+        ]
+      }
+
+      render () {
+        const { items } = this.state;
+        return (
+            <Carousel>
+                {items.map(item => 
+                <div key={item.id} className="icon">
+                    <img src={item.image}/>
+                    {item.title}
+                </div>)}
+            </Carousel>
     )
+}
 }
 
 export default TechnologiesList;
