b23d9d58809ad603337111f4a529ad3e4b33e197
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-04-21 01:47:53 +0100

    :)

--- src/components/TechnologiesList/index.js
+++ src/components/TechnologiesList/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Carousel from 'react-elastic-carousel';
 import './index.css';
 import html from '../../images/html.png'
@@ -21,44 +21,76 @@ import figma from '../../images/figma.png'
 import matlab from '../../images/matlab.png'
 import cli from '../../images/cli.png'
 
+let resetTimeout;
 
-class TechnologiesList extends React.Component {
+const TechnologiesList = () => {
+    
+    let items = [
+        {id: 0, title: 'HTML', image: html},
+        {id: 1, title: 'CSS', image: css},
+        {id: 2, title: 'Javascript', image: js},
+        {id: 3, title: 'React', image: react},
+        {id: 4, title: 'Python', image: python},
+        {id: 5, title: 'Flask', image: flask},
+        {id: 6, title: 'Django', image: django},
+        {id: 7, title: 'Docker', image: docker},
+        {id: 8, title: 'SQL', image: sql},
+        {id: 9, title: 'Postgresql', image: psql},
+        {id: 10, title: 'MongoDB', image: mongo},
+        {id: 11, title: 'Heroku', image: heroku},
+        {id: 12, title: 'Netlify', image: netlify},
+        {id: 13, title: 'VS Code', image: vscode},
+        {id: 14, title: 'PyCharm', image: pycharm},
+        {id: 15, title: 'Command Line Interface', image: cli},
+        {id: 16, title: 'Git', image: git},
+        {id: 17, title: 'Figma', image: figma},
+        {id: 18, title: 'Matlab', image: matlab},
+    ]
+    
+    const itemsPerPage = 1
+    const carouselRef = useRef(null);
+    const totalPages = Math.ceil(items.length / itemsPerPage)
+    let resetTimeout;
+    // state = {
+    //     items: [
+    //       {id: 0, title: 'HTML', image: html},
+    //       {id: 1, title: 'CSS', image: css},
+    //       {id: 2, title: 'Javascript', image: js},
+    //       {id: 3, title: 'React', image: react},
+    //       {id: 4, title: 'Python', image: python},
+    //       {id: 5, title: 'Flask', image: flask},
+    //       {id: 6, title: 'Django', image: django},
+    //       {id: 7, title: 'Docker', image: docker},
+    //       {id: 8, title: 'SQL', image: sql},
+    //       {id: 9, title: 'Postgresql', image: psql},
+    //       {id: 10, title: 'MongoDB', image: mongo},
+    //       {id: 11, title: 'Heroku', image: heroku},
+    //       {id: 12, title: 'Netlify', image: netlify},
+    //       {id: 13, title: 'VS Code', image: vscode},
+    //       {id: 14, title: 'PyCharm', image: pycharm},
+    //       {id: 15, title: 'Command Line Interface', image: cli},
+    //       {id: 16, title: 'Git', image: git},
+    //       {id: 17, title: 'Figma', image: figma},
+    //       {id: 18, title: 'Matlab', image: matlab},
+    //     ]
+    //   }
 
-    state = {
-        items: [
-          {id: 0, title: 'HTML', image: html},
-          {id: 1, title: 'CSS', image: css},
-          {id: 2, title: 'Javascript', image: js},
-          {id: 3, title: 'React', image: react},
-          {id: 4, title: 'Python', image: python},
-          {id: 5, title: 'Flask', image: flask},
-          {id: 6, title: 'Django', image: django},
-          {id: 7, title: 'Docker', image: docker},
-          {id: 8, title: 'SQL', image: sql},
-          {id: 9, title: 'Postgresql', image: psql},
-          {id: 10, title: 'MongoDB', image: mongo},
-          {id: 11, title: 'Heroku', image: heroku},
-          {id: 12, title: 'Netlify', image: netlify},
-          {id: 13, title: 'VS Code', image: vscode},
-          {id: 14, title: 'PyCharm', image: pycharm},
-          {id: 15, title: 'Command Line Interface', image: cli},
-          {id: 16, title: 'Git', image: git},
-          {id: 17, title: 'Figma', image: figma},
-          {id: 18, title: 'Matlab', image: matlab},
-        ]
-      }
-
-      render () {
-        const { items } = this.state;
         return (
             <>
-            <Carousel enableAutoPlay={true} autoPlaySpeed={3000} 
-            onNextEnd={(currentItem, pageIndex) => {
-                if (pageIndex == 18) {
-                    // fix this if you can someday :(
-                    // isRTL={true}
-                }
-              }}>
+            <Carousel 
+                ref={carouselRef}
+                enableAutoPlay={true} 
+                autoPlaySpeed={3000}
+                disableArrowsOnEnd={false}
+                onNextEnd={({ index }) => {
+                    clearTimeout(resetTimeout)
+                    if (index + 1 === totalPages) {
+                    resetTimeout = setTimeout(() => {
+                        carouselRef.current.goTo(0)
+                    }, 1500) // same time
+                    }
+                }}
+            >
                 {items.map(item => 
                 <div key={item.id} className="icon">
                     <img src={item.image}/>
@@ -68,6 +100,6 @@ class TechnologiesList extends React.Component {
         </>
     )
 }
-}
+
 
 export default TechnologiesList;
